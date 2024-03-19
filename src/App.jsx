d346b9c25b328ae7b0563b6c780dd8f14ed0de9e
Nguyen Tuan <norsinlight@gmail.com>
2024-03-19 13:48:46 +0700

    update navlink and error pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,18 @@ import ProductList from './pages/products/ProductList';
 import ProductEdit from './pages/products/ProductEdit';
 import ProductAdd from './pages/products/ProductAdd';
 import HomeContent from './layouts/homeContent/HomeContent';
-// import Nav from './layouts/nav/nav';
 import Home from "./pages/home/Home";
+import ErrorPage from './pages/errors/error-page'; 
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} >
+      <Route path="/" element={<Home />}>
         <Route path="/products" element={<ProductList />} />
         <Route path="/products/:productId/edit" element={<ProductEdit />} />
         <Route path="/products/add" element={<ProductAdd/>} />
         <Route path="/" element={<HomeContent />} />
+        <Route path="*" element={<ErrorPage />} />
       </Route>
     </Routes>
   );
--- src/layouts/nav/Nav.jsx
+++ src/layouts/nav/Nav.jsx
@@ -7,6 +7,10 @@ const nav = () => {
         <ul  >
             <li><NavLink to={'/'}>Home</NavLink></li>
             <li><NavLink to={'/products'}>Product List</NavLink></li>
+            <li><NavLink to={'/categories'}>Categories</NavLink></li>
+            <li><NavLink to={'/about'}>About Us</NavLink></li>
+            <li><NavLink to={'/contact'}>Contact Us</NavLink></li>
+
             {/* <li><NavLink to={'/d'}>ffdfdf</NavLink> </li> */}
         </ul>
     </div>
